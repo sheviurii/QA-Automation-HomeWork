0d32c69f9d437599a9a7fd3f880a0a74d1204176
Юрий <sheviurii@gmail.com>
2019-05-28 22:27:15 +0300

    debug

--- src/main/java/Lesson_1/Task4.java
+++ src/main/java/Lesson_1/Task4.java
@@ -2,12 +2,15 @@ package Lesson_1;
 
 public class Task4 {
 
-    public static double a = 5;
-    public static double b = 10;
+    public static int a = 5;
+    public static int b = 10;
 
     public static void main(String[] args) {
 
-        double result = a / b;
+        double c = (int)a;
+        double d = (int)b;
+
+        double result = c / d;
 
         System.out.println(result);
     }
