66c6572751bafdfc15be3efa7e3673d6a2a5599a
Юрий <sheviurii@gmail.com>
2019-06-03 15:27:59 +0300

    Create branch and push on git

--- src/main/java/Lesson_2/Tasks1_3.java
+++ src/main/java/Lesson_2/Tasks1_3.java
@@ -2,6 +2,7 @@ package Lesson_2;
 
 public class Tasks1_3 {
     public static void main(String[] args) {
+        System.out.println("Create the branch");
         System.out.println("This class for push to the git another branch");
     }
 }
